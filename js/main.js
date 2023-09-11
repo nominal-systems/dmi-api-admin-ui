a7c1cbe941bbb0fb23314a1bd9317793c0fe5616
monsieurbelbo <gonzalo.bellver@gmail.com>
2023-09-11 14:59:33 +0200

    Sidebar

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 import 'regenerator-runtime/runtime'
 import Alpine from 'alpinejs'
-import {Modal} from 'flowbite'
 
-const DMI_API_URL = 'http://localhost:3000'
+const DMI_API_URL = process.env.API_URL
 
 function getThemeFromLocalStorage() {
   // if user already changed the theme, use it
@@ -117,23 +116,24 @@ window.data = {
   menu: [
     {
       slug: 'dashboard',
-      url: '/ui',
+      url: '/',
       name: 'Dashboard',
-      icon: 'M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6'
+      icon: 'M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6',
     },
     {
       slug: 'integrations',
-      url: '/ui/integrations',
+      url: '/integrations',
       name: 'Integrations',
       icon: 'M7.5 21L3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5'
     },
     {
       slug: 'events',
-      url: '/ui/events',
+      url: '/events',
       name: 'Events',
       icon: 'M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z'
     }
   ],
+
 
   // Integrations
   integrations: [],
@@ -153,7 +153,7 @@ window.data = {
   },
   async updateIntegrationStatus() {
     this.isIntegrationStatusUpdating = true
-    apiPost(`/admin/integrations/${this.integrationId}/${this.operation}`, null, (body) => {
+    apiPost(`${DMI_API_URL}/admin/integrations/${this.integrationId}/${this.operation}`, null, (body) => {
       this.isIntegrationStatusUpdating = false
       this.closeModal()
     })
@@ -162,7 +162,7 @@ window.data = {
   // Events
   events: [],
   async fetchEvents() {
-    apiGet('/admin/events', (body) => {
+    apiGet(`${DMI_API_URL}/admin/events`, (body) => {
       this.events = body.map(event => {
         return {
           ...event,
