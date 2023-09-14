8c1a2c50c332b12826f961faadbace9825adbaf0
monsieurbelbo <gonzalo.bellver@gmail.com>
2023-09-14 14:43:25 +0200

    Refs table

--- js/refs.js
+++ js/refs.js
@@ -2,7 +2,7 @@ import { Tabs } from 'flowbite'
 import { apiGet } from "./api-client";
 
 const DMI_API_URL = process.env.API_URL
-const options = {
+const tabOptions = {
   defaultTabId: 'sexes',
   activeClasses: 'text-blue-600 hover:text-blue-600 dark:text-blue-500 dark:hover:text-blue-400 border-blue-600 dark:border-blue-500',
   inactiveClasses: 'text-gray-500 hover:text-gray-600 dark:text-gray-400 border-gray-100 hover:border-gray-300 dark:border-gray-700 dark:hover:text-gray-300',
@@ -24,14 +24,49 @@ const tabElements = [
     targetEl: document.querySelector('#breeds')
   }
 ]
+const $modalTarget = document.getElementById('refsModal');
+const modalOptions = {
+  placement: 'bottom-right',
+  backdrop: 'dynamic',
+  backdropClasses: 'bg-gray-900 bg-opacity-50 dark:bg-opacity-80 fixed inset-0 z-40',
+  closable: true,
+  onHide: () => {
+    console.log('modal is hidden');
+  },
+  onShow: () => {
+    console.log('modal is shown');
+  },
+  onToggle: () => {
+    console.log('modal has been toggled');
+  }
+};
 
 export const refs = {
   tabs: [],
-  items: [],
   referenceData: {
     sexes: [],
     species: [],
     breeds: []
+  },
+  providers: [],
+  modal: new Modal($modalTarget, modalOptions),
+  editingRef: {},
+  editingRefMappings: [],
+  openModal(ref) {
+    this.editingRef = ref
+    this.editingRefMappings = []
+    this.providers.forEach((provider) => {
+      this.editingRefMappings.push({
+        provider: provider.id,
+        ref: ref.providerRef.find((providerRef) => providerRef.provider.id === provider.id)
+      })
+    })
+    this.modal.show()
+  },
+  async fetchProviders() {
+    await apiGet(`${DMI_API_URL}/admin/providers`, (body) => {
+      this.providers = body
+    })
   },
   async fetchReferenceData() {
     await Promise.all([
@@ -47,7 +82,8 @@ export const refs = {
     ])
   },
   async init() {
+    this.tabs = new Tabs(tabElements, tabOptions)
     await this.fetchReferenceData()
-    this.tabs = new Tabs(tabElements, options);
+    await this.fetchProviders()
   }
 }
