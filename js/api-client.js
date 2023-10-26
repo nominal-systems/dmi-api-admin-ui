e7b2bd008786f51aafdc69ed64b53b8650314957
monsieurbelbo <gonzalo.bellver@gmail.com>
2023-10-26 10:34:36 +0200

    External Requests table WIP

--- js/api-client.js
+++ js/api-client.js
@@ -3,93 +3,93 @@ import { getTokenFromLocalStorage, unsetTokenFromLocalStorage } from "./auth";
 const BASE_URL = `${process.env.API_URL}/admin`
 
 export function apiPost(url, body, next) {
-    const req = {
-        method: 'POST',
-        headers: {
-            Authorization: `Bearer ${getTokenFromLocalStorage()}`
-        }
+  const req = {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${getTokenFromLocalStorage()}`
     }
+  }
 
-    if (body !== null) {
-        req.headers['Content-Type'] = 'application/json'
-        req.body = body
-    }
+  if (body !== null) {
+    req.headers['Content-Type'] = 'application/json'
+    req.body = body
+  }
 
-    return fetch(url, req)
-        .then(response => response.json())
-        .then(next)
+  return fetch(url, req)
+    .then(response => response.json())
+    .then(next)
 }
 
 export function apiGet(url, next) {
-    return fetch(url, {
-        headers: new Headers({
-            Authorization: `Bearer ${getTokenFromLocalStorage()}`
-        })
-    }).then(res => res.json())
-        .then(res => {
-            if (res.statusCode === 401 || res.statusCode === 403) {
-                unsetTokenFromLocalStorage()
-                window.location.href = '/login'
-            }
-            next(res)
-        })
+  return fetch(url, {
+    headers: new Headers({
+      Authorization: `Bearer ${getTokenFromLocalStorage()}`
+    })
+  }).then(res => res.json())
+    .then(res => {
+      if (res.statusCode === 401 || res.statusCode === 403) {
+        unsetTokenFromLocalStorage()
+        window.location.href = '/login'
+      }
+      next(res)
+    })
 }
 
 export const getEvents = async () => {
-    let events = []
-    await apiGet(`${BASE_URL}/events`, (body) => {
-        events = body
-    })
-    return events
+  let events = []
+  await apiGet(`${BASE_URL}/events`, (body) => {
+    events = body
+  })
+  return events
 }
 export const getIntegrations = async (next) => {
-    let integrations = []
-    await apiGet(`${BASE_URL}/integrations`, (body) => {
-        integrations = body
-    })
-    return integrations
+  let integrations = []
+  await apiGet(`${BASE_URL}/integrations`, (body) => {
+    integrations = body
+  })
+  return integrations
 }
 export const getIntegrationForProvider = async (providerId) => {
-    let integration = {}
-    await apiGet(`${BASE_URL}/integrations?providerId=${providerId}`, (body) => {
-        integration = body[0]
-    })
-    return integration
+  let integration = {}
+  await apiGet(`${BASE_URL}/integrations?providerId=${providerId}`, (body) => {
+    integration = body[0]
+  })
+  return integration
 }
 export const getProviders = async () => {
-    let providers = []
-    await apiGet(`${BASE_URL}/providers`, (body) => {
-        providers = body
-    })
-    return providers
+  let providers = []
+  await apiGet(`${BASE_URL}/providers`, (body) => {
+    providers = body
+  })
+  return providers
 }
 
 export const getProvider = async (id) => {
-    let provider = {}
-    await apiGet(`${BASE_URL}/providers/${id}`, (body) => {
-        provider = body
-    })
-    return provider
+  let provider = {}
+  await apiGet(`${BASE_URL}/providers/${id}`, (body) => {
+    provider = body
+  })
+  return provider
 }
 
 export const getProviderRefs = async (id, type) => {
-    let refs = []
-    await apiGet(`${BASE_URL}/providers/${id}/refs/${type}`, (body) => {
-        refs = body
-    })
-    return refs
+  let refs = []
+  await apiGet(`${BASE_URL}/providers/${id}/refs/${type}`, (body) => {
+    refs = body
+  })
+  return refs
 }
 
 export const syncProviderRefs = async (provider, type, integrationId, next) => {
-    await apiPost(`${BASE_URL}/refs/sync/${provider}/${type}?integrationId=${integrationId}`, null, (body) => {
-        next(body)
-    })
+  await apiPost(`${BASE_URL}/refs/sync/${provider}/${type}?integrationId=${integrationId}`, null, (body) => {
+    next(body)
+  })
 }
 
-export const getExternalRequests = async () => {
-    let requests = []
-    await apiGet(`${BASE_URL}/external-requests`, (body) => {
-        requests = body
-    })
-    return requests
+export const getExternalRequests = async (page, limit) => {
+  let result = {}
+  await apiGet(`${BASE_URL}/external-requests?page=${page}&limit=${limit}`, (body) => {
+    result = body
+  })
+  return result
 }
--- js/external-requests.js
+++ js/external-requests.js
@@ -1,12 +1,28 @@
 import { getExternalRequests } from './api-client'
 
+const PAGE_SIZE = 25
+
 export const externalRequests = () => {
   return {
+    page: null,
+    pageSize: PAGE_SIZE,
+    pagesTotal: null,
+    resultsStart: null,
+    resultsEnd: null,
+    total: null,
     requests: [],
+    async getPage(page) {
+      this.page = page
+      const requests = await getExternalRequests(this.page, this.pageSize)
+      this.requests = requests.data
+      this.total = requests.total
+      this.page = requests.page
+      this.resultsStart = this.page * this.pageSize - this.pageSize + 1
+      this.resultsEnd = Math.min(this.page * this.pageSize, this.total)
+      this.pagesTotal = Math.ceil(this.total / this.pageSize)
+    },
     async init() {
-      console.log('externalRequests.init()') // TODO(gb): remove trace
-      this.requests = await getExternalRequests()
-      console.log(`this.requests= ${JSON.stringify(this.requests, null, 2)}`) // TODO(gb): remove trace
+      await this.getPage(1)
     }
   }
 }
