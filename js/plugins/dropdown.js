600a5127f8777af2e69e47da078deee54fabc5ca
monsieurbelbo <gonzalo.bellver@gmail.com>
2024-08-12 14:19:46 +0200

    Refactored dropdown

--- js/plugins/dropdown.js
+++ js/plugins/dropdown.js
@@ -6,74 +6,75 @@ export default function (Alpine) {
   }
 
   Alpine.directive('dropdown', (el, { value, modifiers, expression }, { effect, evaluate, evaluateLater, cleanup }) => {
-    if (value === 'button') {
-      return // This will get handled by the main directive...
+    if (!value) {
+      const options = Object.assign({}, defaultOptions, evaluate(expression))
+      handleRoot(el, Alpine, options)
+    } else if (value === 'button') {
+    } else if (value === 'menu') {
+    } else if (value === 'item') {
+      handleItem(el, Alpine)
+    } else if (value === 'toggle') {
+      handleToggle(el, Alpine)
     }
+  })
+}
 
-    if (value === 'menu') {
-      return // This will get handled by the main directive...
-    }
+function handleRoot(el, Alpine, options) {
+  Alpine.bind(el, {
+    'x-data'() {
+      return {
+        init() {
+          const $buttonEl = el.querySelector('[x-dropdown\\:button]')
+          const $menuEl = el.querySelector('[x-dropdown\\:menu]')
+          let $itemEl = undefined
+          setTimeout(() => {
+            $itemEl = el.querySelectorAll('[x-dropdown\\:item] input')
+          })
 
-    if (value === 'item') {
-      return // This will get handled by the main directive...
-    }
-
-    if (value === 'toggle') {
-      return // This will get handled by the main directive
-    }
-
-    // Selectors
-    const $buttonEl = el.querySelector('[x-dropdown\\:button]')
-    const $menuEl = el.querySelector('[x-dropdown\\:menu]')
-    const $toggleEl = el.querySelector('[x-dropdown\\:toggle] input')
-    let $itemEl = null
-    document.addEventListener('alpine:initialized', () => {
-      $itemEl = el.querySelectorAll('[x-dropdown\\:item] input')
-      for (const item of $itemEl) {
-        item.addEventListener('click', (e) => {
-          this.state.dirty = isDirty($itemEl)
-          this.state.toggle = countChecked($itemEl) === $itemEl.length
-        })
+          new Dropdown($menuEl, $buttonEl, {
+            placement: 'bottom',
+            triggerType: 'click',
+            offsetSkidding: 0,
+            offsetDistance: 10,
+            delay: 300,
+            ignoreClickOutsideClass: false
+          })
+          el.addEventListener('selectionChanged', (e) => {
+            this.dirty = isDirty($itemEl)
+            this.toggle = countChecked($itemEl) === $itemEl.length
+          })
+          el.addEventListener('selectAllToggled', (e) => {
+            $itemEl.forEach(item => {
+              item.checked = this.toggle
+            })
+            this.dirty = isDirty($itemEl)
+          })
+        },
+        label: options.label,
+        toggleLabel: options.toggleLabel,
+        dirty: false,
+        toggle: false,
+        items: options.items
       }
-    })
+    }
+  })
+}
 
-    // Dropdown data
-    const options = Object.assign({}, defaultOptions, evaluate(expression))
-    this.label = options.label
-    this.state = Alpine.reactive({
-      toggle: false,
-      dirty: false
-    })
-    this.items = Alpine.reactive(options.items)
-    this.toggleLabel = options.toggleLabel
+function handleToggle(el, Alpine) {
+  const $toggleInput = el.querySelector('input')
+  Alpine.bind($toggleInput, {
+    '@click'() {
+      this.toggle = !this.toggle
+      this.$dispatch('selectAllToggled', { el: this.$el })
+    }
+  })
+}
 
-    // Toggle
-    $toggleEl.addEventListener('click', (e) => {
-      this.toggle = $toggleEl.checked
-      $itemEl.forEach(item => {
-        item.checked = this.toggle
-      })
-      this.state.dirty = isDirty($itemEl)
-    })
-
-    // Dropdown
-    new Dropdown($menuEl, $buttonEl, {
-      placement: 'bottom',
-      triggerType: 'click',
-      offsetSkidding: 0,
-      offsetDistance: 10,
-      delay: 300,
-      ignoreClickOutsideClass: false,
-      onHide: () => {
-        console.log('dropdown has been hidden')
-      },
-      onShow: () => {
-        console.log('dropdown has been shown')
-      },
-      onToggle: () => {
-        console.log('dropdown has been toggled')
-      },
-    })
+function handleItem(el, Alpine) {
+  Alpine.bind(el.querySelector('input'), {
+    '@click'() {
+      this.$dispatch('selectionChanged', { el: this.$el })
+    }
   })
 }
 
