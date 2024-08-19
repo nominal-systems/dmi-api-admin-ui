6bebbbcb865e3caaf6e5021084afdb0fb19aa512
monsieurbelbo <gonzalo.bellver@gmail.com>
2024-08-19 15:43:39 +0200

    Fixed pagination loop

--- js/plugins/table.js
+++ js/plugins/table.js
@@ -10,6 +10,7 @@ export default (opts, getPageFn) => ({
   resultsStart: null,
   resultsEnd: null,
   pagesTotal: null,
+  nav: null,
   getPageFn: getPageFn,
   async getPage(page) {
     const response = await this.getPageFn(page || this.page, this.pageSize)
@@ -19,6 +20,7 @@ export default (opts, getPageFn) => ({
     this.resultsStart = this.page * this.pageSize - this.pageSize + 1
     this.resultsEnd = Math.min(this.page * this.pageSize, this.total)
     this.pagesTotal = Math.ceil(this.total / this.pageSize)
+    this.nav = navPages(this.page, this.pagesTotal, this.pagesMax)
   },
   async nextPage() {
     if (this.page < this.pagesTotal) {
@@ -34,3 +36,25 @@ export default (opts, getPageFn) => ({
     await this.getPage(1)
   }
 })
+
+function navPages(page, pagesTotal, pagesMax) {
+  const half = Math.floor(pagesMax / 2)
+  let start = Math.max(1, page - half)
+  let end = Math.min(pagesTotal, page + half)
+
+  // Adjust the start and end if the pagesMax window goes out of bounds
+  if (end - start + 1 < pagesMax) {
+    if (start === 1) {
+      end = Math.min(pagesTotal, start + pagesMax - 1)
+    } else if (end === pagesTotal) {
+      start = Math.max(1, end - pagesMax + 1)
+    }
+  }
+
+  let pages = [];
+  for (let i = start; i <= end; i++) {
+    pages.push(i)
+  }
+
+  return pages
+}
