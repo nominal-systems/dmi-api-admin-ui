1643bf43db6a9446c04dea99fc4d63a5335187f6
monsieurbelbo <gonzalo.bellver@gmail.com>
2024-10-18 14:32:31 +0200

    Transaction Logs improvements

--- js/transaction-logs.js
+++ js/transaction-logs.js
@@ -1,4 +1,4 @@
-import { getQueryParams } from './common/utils'
+import { getQueryParams, mapHttpStatusColor, mapHttpStatusText } from './common/utils'
 import { getTransactionLogs } from './api-client'
 
 export default () => {
@@ -11,8 +11,57 @@ export default () => {
       const transactionLogs = await getTransactionLogs(this.accessionId)
       transactionLogs.forEach(log => {
         log.timestamp = new Date(log.timestamp).toLocaleString()
+        log.message = buildMessage(log)
+        log.tag = buildTag(log)
       })
       this.logs = transactionLogs
     }
   }
+}
+
+function buildMessage(log) {
+  let message = ''
+  const data = log.data
+  if (log.type === 'event') {
+    let operation = ''
+    if (data.type.includes('created')) {
+      operation = 'created'
+    } else if (data.type.includes('updated')) {
+      operation = 'updated'
+
+    }
+    let entity = ''
+    switch (data.namespace) {
+      case 'orders':
+        entity = `Order/${data.data.orderId}`
+        break
+      case 'reports':
+        entity = `Report/${data.data.reportId}`
+        break
+    }
+    message = `${operation} ${entity}`
+  }
+  return message
+}
+
+function buildTag(log) {
+  const tag = {
+    label: '',
+    color: 'blue'
+  }
+  switch (log.type) {
+    case 'event':
+      tag.label = log.data.namespace === 'orders' ?
+        log.data.data.order.status :
+        log.data.data.report.status
+      if (['COMPLETED', 'FINAL'].includes(log.data.status)) {
+        tag.color = 'green'
+      }
+      break
+    case 'external-request':
+      tag.label = `${log.data.status} ${mapHttpStatusText(log.data.status)}`
+      tag.color = mapHttpStatusColor(log.data.status)
+      break
+  }
+  return tag
 }
