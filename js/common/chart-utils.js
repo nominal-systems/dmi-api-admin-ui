0822431fa0c1f10741b484d7b7a5dfc3065264fe
monsieurbelbo <gonzalo.bellver@gmail.com>
2024-10-30 11:33:26 +0100

    Including today in dashboard charts

--- js/common/chart-utils.js
+++ js/common/chart-utils.js
@@ -1,3 +1,5 @@
+import moment from 'moment'
+
 export function createTimeSeries(data, startDate, endDate, options = {}) {
   const defaultOptions = {
     granularity: 'day',
@@ -14,7 +16,7 @@ export function createTimeSeries(data, startDate, endDate, options = {}) {
     return { name: s.label, id: s.id, data: [] }
   })
   const start = new Date(startDate)
-  const end = new Date(endDate)
+  const end = moment(new Date(endDate)).endOf('day').toDate()
   const timeSeries = [];
   while (start <= end) {
     let x;
