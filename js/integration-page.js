5c36a9f549baa744e7306fa0591a1c9375253cab
monsieurbelbo <gonzalo.bellver@gmail.com>
2025-06-16 15:24:11 +0200

    Allow start/stop/restart integrations from Integration page.

--- js/integration-page.js
+++ js/integration-page.js
@@ -1,13 +1,55 @@
 import Alpine from 'alpinejs'
 import { getIdFromPath, getIntegrationConfig } from './common/utils'
-import { getIntegration } from './api-client'
+import { getIntegration, updateIntegrationStatus } from './api-client'
+import modal from './plugins/modal'
 
 export const integrationPage = {
   integration: {},
+  actionModal: modal({
+    ref: 'integrationModal',
+    data: {
+      inProgress: false,
+      operation: null,
+      integration: null
+    },
+    onShow: (self, data) => {
+      data.error = null
+      data.inProgress = false
+    },
+    onHide: async (self) => {
+      self.operation = null
+      if (self.page) {
+        await self.page.loadIntegration()
+      }
+    }
+  }),
   async init() {
+    this.actionModal.page = this
+    await this.loadIntegration()
+  },
+  async loadIntegration() {
     const integration = await getIntegration(getIdFromPath())
     Alpine.store('title').set(`Integration ${integration.id}`)
-    integration.color = (getIntegrationConfig(integration.status)).color
+    const cfg = getIntegrationConfig(integration.status)
+    integration.color = cfg.color
+    integration.operations = cfg.operations
     this.integration = integration
+  },
+  async updateIntegrationStatus(data) {
+    data.error = null
+    data.inProgress = true
+    await updateIntegrationStatus(data.integration.id, data.operation)
+      .then(() => {
+        data.inProgress = false
+        this.actionModal.close()
+        Alpine.store('alert')
+          .set('info', `Integration ${data.integration.id} ${data.operation}${data.operation === 'stop' ? 'p' : ''}ed.`)
+      })
+      .catch(err => {
+        data.inProgress = false
+        data.error = {
+          message: err.body.error
+        }
+      })
   }
 }
