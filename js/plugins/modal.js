cfcaea085d9ccec9c1576d7ac858b5caa6e62f90
monsieurbelbo <gonzalo.bellver@gmail.com>
2026-03-17 12:58:30 +0100

    fix: modal plugin init

--- js/plugins/modal.js
+++ js/plugins/modal.js
@@ -6,33 +6,35 @@ export default (opts) => ({
   onShow: opts.onShow,
   onHide: opts.onHide,
   data: opts.data || {},
-  async init() {
-    const modalOptions = {
-      placement: 'bottom-right',
-      backdrop: 'dynamic',
-      backdropClasses: 'bg-gray-900 bg-opacity-50 dark:bg-opacity-80 fixed inset-0 z-40',
-      closable: true,
-      onShow: () => {
-        if (this.onShow) {
-          this.onShow(this, this.data)
+  _initModal() {
+    if (!this.modal) {
+      this.modal = new Modal(this.$refs[this.ref], {
+        placement: 'bottom-right',
+        backdrop: 'dynamic',
+        backdropClasses: 'bg-gray-900 bg-opacity-50 dark:bg-opacity-80 fixed inset-0 z-40',
+        closable: true,
+        onShow: () => {
+          if (this.onShow) {
+            this.onShow(this, this.data)
+          }
+        },
+        onHide: () => {
+          if (this.onHide) {
+            this.onHide(this, this.data)
+          }
         }
-      },
-      onHide: () => {
-        if (this.onHide) {
-          this.onHide(this, this.data)
-        }
-      }
+      })
     }
-
-    this.modal = new Modal(this.$refs[this.ref], modalOptions)
   },
   async open(data) {
+    this._initModal()
     if (data) {
       Object.assign(this.data, data)
     }
     this.modal.show()
   },
   async close() {
+    this._initModal()
     this.modal.hide()
   }
 })
